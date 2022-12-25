11b8722c11f2da984fd11ee6ed359e4c059d6921
TosiaDan <antonina.ivanets@gmail.com>
2022-12-25 23:24:01 +0100

    corrects loader

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -3,8 +3,8 @@ import styled from '@emotion/styled';
 export const LoaderContainer = styled.div`
   display: inline-block;
   position: fixed;
-  top: 50%;
-  left: 50%;
+  top: calc(50% - 50px);
+  left: calc(50% - 50px);
   width: 100px;
   height: 100px;
   border: 5px solid rgba(255, 255, 255);
